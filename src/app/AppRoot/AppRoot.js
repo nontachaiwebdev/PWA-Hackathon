9f5ea62450895e1b83ced4591417e1ab8622bfa2
Nontchai Tantiponsawas <nontchai@Nontchais-MacBook-Pro.local>
2017-06-24 09:55:05 +0700

    Add facebook Login

--- src/app/AppRoot/AppRoot.js
+++ src/app/AppRoot/AppRoot.js
@@ -4,6 +4,12 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import { Provider } from 'react-redux'
 import reducer from './../../reducers'
 import App from './../../screens/App'
+import database from './../../model/firebase'
+
+database.ref('test').once('value', snap => { 
+  const invite = snap.val();
+  console.log(invite)
+})
 
 let store = createStore(
   reducer,
---
+++ src/model/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+
+const config = {
+    apiKey: "AIzaSyA47tsECXDqyyyxndig39wyIZrCa4i-aT8",
+    authDomain: "pwa-hackathon-db740.firebaseapp.com",
+    databaseURL: "https://pwa-hackathon-db740.firebaseio.com",
+    projectId: "pwa-hackathon-db740",
+    storageBucket: "pwa-hackathon-db740.appspot.com",
+    messagingSenderId: "481822090396"
+};
+
+firebase.initializeApp(config)
+const database = firebase.database()
+
+export default database
