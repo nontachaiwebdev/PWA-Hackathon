ff257ec7a5f4cd96c52404fe1281379b3b8645d6
Nontchai Tantiponsawas <nontchai@Nontchais-MacBook-Pro.local>
2017-06-24 10:55:33 +0700

    add board header

--- src/screens/Board/Board.js
+++ src/screens/Board/Board.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react'
+import './Board.css'
+import addIcon from './../../asset/plus.png'
 
 export default class Board extends Component{
   render () {
     return (
-      <div>This is a board</div>
+      <div className='boardContainer'>
+        <div className='boardHeader'>
+          <p>Sprint Board</p>
+	  <div className='addIcon'>
+	    <img src={addIcon} />
+	  </div>
+	</div>
+      </div>
     )
   }
 }
