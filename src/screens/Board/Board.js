e62c0af15869a06ea611985b2c3b447f89cca30d
sumetma-in <sumet_ma-in@hotmail.com>
2017-06-24 15:44:20 +0700

    formate(Board): standard

--- src/screens/Board/Board.js
+++ src/screens/Board/Board.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import AppBar from 'material-ui/AppBar'
 import Drawer from 'material-ui/Drawer'
 import MenuItem from 'material-ui/MenuItem'
@@ -8,63 +8,50 @@ import injectTapEventPlugin from 'react-tap-event-plugin'
 import './Board.css'
 
 injectTapEventPlugin();
-export default class Board extends Component{
+export default class Board extends Component {
   state = {
     isMenu: false
   }
 
   handleOpenMenuTouch = () => {
-    const { isMenu } = this.state
+    const {isMenu} = this.state
     this.setState({
       isMenu: !isMenu
     })
   }
 
-  render () {
-    const { isMenu } = this.state
-    const { handleOpenMenuTouch } = this
+  render() {
+    const {isMenu} = this.state
+    const {handleOpenMenuTouch} = this
     return (
       <div>
         <AppBar
-          title = "Sprint Board"
-          iconClassNameRight = "muidocs-icon-navigation-expand-more"
-	  iconElementRight={<FlatButton label="Add" />}
-	  onLeftIconButtonTouchTap = { () => handleOpenMenuTouch() }
-        />
-        <Drawer
-          docked = {false}
-          width = {200}
-          open = {isMenu}
-          onRequestChange = { () => handleOpenMenuTouch() }
-        >
-          <MenuItem onTouchTap = { this.handleClose }>Menu Item</MenuItem>
-          <MenuItem onTouchTap = { this.handleClose }>Menu Item 2</MenuItem>
+          title="Sprint Board"
+          iconClassNameRight="muidocs-icon-navigation-expand-more"
+          iconElementRight={< FlatButton label = "Add" />}
+          onLeftIconButtonTouchTap= { () => handleOpenMenuTouch() }/>
+        <Drawer docked={false} width={200} open={isMenu} onRequestChange= { () => handleOpenMenuTouch() }>
+          <MenuItem onTouchTap={this.handleClose}>Menu Item</MenuItem>
+          <MenuItem onTouchTap={this.handleClose}>Menu Item 2</MenuItem>
         </Drawer>
-	<div className='cardContainer'>
+        <div className='cardContainer'>
           <Card>
-	    <CardHeader
-	      title="Without Avatar"
-	      subtitle="Subtitle"
-	      actAsExpander={true}
-	      showExpandableButton={true} />
-	    <CardText expandable={true}>
-	      <p>Date: 12/02/2016 - 15/02/2016</p>
-	      <p>Cards: 100</p>
-	      <p>Done: 20</p>
-	      <p>Expire: 2 Day</p> 
-	    </CardText>
-	    <CardActions 
-	      expandable={true}
-	      style={{'textAlign': 'right'}} >
-	      <FlatButton 
-	        label="Detail" 
-	        fullWidth={true} 
-	        primary={true}
-	        backgroundColor={'#4FC3F7'}
-	        style={{'color': '#FFFFFF'}}  />
-	    </CardActions>
-	  </Card>
-	</div>
+            <CardHeader title="Without Avatar" subtitle="Subtitle" actAsExpander={true} showExpandableButton={true}/>
+            <CardText expandable={true}>
+              <p>Date: 12/02/2016 - 15/02/2016</p>
+              <p>Cards: 100</p>
+              <p>Done: 20</p>
+              <p>Expire: 2 Day</p>
+            </CardText>
+            <CardActions expandable={true} style={{
+              'textAlign': 'right'
+            }}>
+              <FlatButton label="Detail" fullWidth={true} primary={true} backgroundColor={'#4FC3F7'} style={{
+                'color': '#FFFFFF'
+              }}/>
+            </CardActions>
+          </Card>
+        </div>
       </div>
     )
   }
