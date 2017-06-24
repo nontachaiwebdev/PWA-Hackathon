a6bf735f37018f0f2e3b013f081f50485ab997fe
Nontchai Tantiponsawas <nontchai@Nontchais-MacBook-Pro.local>
2017-06-24 17:40:20 +0700

    auth firebase

--- src/model/firebase.js
+++ src/model/firebase.js
@@ -1,32 +1,4 @@
 import firebase from 'firebase'
-import firebaseAuth from 'firebase-auth'
-
-const provider = new firebase.auth.GoogleAuthProvider()
-
-
-const options = {
-  rootRef: "rootRef",
-  secretKey: process.env.FIREBASE_SECRET_KEY,
-  UID: 'uid',
-  admin: true || false //true or false
-}
-
-firebase.auth().signInWithPopup(provider).then(function(result) {
-  // This gives you a Google Access Token. You can use it to access the Google API.
-  var token = result.credential.accessToken;
-  // The signed-in user info.
-  var user = result.user;
-  // ...
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-  // ...
-});
 
 const config = {
     apiKey: "AIzaSyA47tsECXDqyyyxndig39wyIZrCa4i-aT8",
@@ -38,6 +10,13 @@ const config = {
 };
 
 firebase.initializeApp(config)
+
+firebase.auth().signInWithEmailAndPassword('testapi@firebase.com', '123456789').catch(function(error) {
+  const errorCode = error.code
+  const errorMessage = error.message
+  console.error(errorCode)
+  console.error(errorMessage)
+}); 
 
 const database = firebase.database()
 
