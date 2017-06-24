60b43c8066cdd723690ac3db262d845e0dd13460
Nontchai Tantiponsawas <nontchai@Nontchais-MacBook-Pro.local>
2017-06-24 15:06:47 +0700

    add layout design

--- src/screens/Board/Board.js
+++ src/screens/Board/Board.js
@@ -1,15 +1,65 @@
 import React, { Component } from 'react'
 import AppBar from 'material-ui/AppBar'
+import Drawer from 'material-ui/Drawer'
+import MenuItem from 'material-ui/MenuItem'
+import FlatButton from 'material-ui/FlatButton'
+import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card'
 import injectTapEventPlugin from 'react-tap-event-plugin'
-import addIcon from './../../asset/plus.png'
+import './Board.css'
+
 injectTapEventPlugin();
 export default class Board extends Component{
+  state = {
+    isMenu: false
+  }
+
+  handleOpenMenuTouch = () => {
+    const { isMenu } = this.state
+    this.setState({
+      isMenu: !isMenu
+    })
+  }
+
   render () {
+    const { isMenu } = this.state
+    const { handleOpenMenuTouch } = this
     return (
-      <AppBar
-        title="Title"
-        iconClassNameRight="muidocs-icon-navigation-expand-more"
-      />
+      <div>
+        <AppBar
+          title = "Sprint Board"
+          iconClassNameRight = "muidocs-icon-navigation-expand-more"
+	  iconElementRight={<FlatButton label="Add" />}
+	  onLeftIconButtonTouchTap = { () => handleOpenMenuTouch() }
+        />
+        <Drawer
+          docked = {false}
+          width = {200}
+          open = {isMenu}
+          onRequestChange = { () => handleOpenMenuTouch() }
+        >
+          <MenuItem onTouchTap = { this.handleClose }>Menu Item</MenuItem>
+          <MenuItem onTouchTap = { this.handleClose }>Menu Item 2</MenuItem>
+        </Drawer>
+	<div className='cardContainer'>
+          <Card>
+	    <CardHeader
+	      title="Without Avatar"
+	      subtitle="Subtitle"
+	      actAsExpander={true}
+	      showExpandableButton={true} />
+	    <CardActions>
+	      <FlatButton label="Action1" />
+	      <FlatButton label="Action2" />
+	    </CardActions>
+	    <CardText expandable={true}>
+	      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+	      Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+	      Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+	      Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+	    </CardText>
+	  </Card>
+	</div>
+      </div>
     )
   }
 }
